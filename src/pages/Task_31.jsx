3324f24c7472dea4a4083ac6c3826a4467311b80
Vasiliy <wkurganow@gmail.com>
2023-02-19 14:49:18 +0700

    Заливая проект на гитХаб

---
+++ src/pages/Task_31.jsx
@@ -0,0 +1,25 @@
+import condition_task31 from "./../img/tasks/condition_task31.png";
+const Task_31 = () => {
+  /** Задача - Интервалы и таймауты
+   *
+   * Используйте следующие встроенные функции:
+   * setTimeout
+   * setInterval
+   * clearInterval
+   *
+   * Нужно вывести в консоль ТОЛЬКО 5 сообщений с интервалом 2 секунды
+   * "Сообщение номер 1"
+   * "Сообщение номер 2"
+   * "Сообщение номер 3"
+   * "Сообщение номер 4"
+   * "Сообщение номер 5"
+   */
+  return (
+    <div>
+      <div>
+        <img src={condition_task31} alt="" />
+      </div>
+    </div>
+  );
+};
+export default Task_31;
